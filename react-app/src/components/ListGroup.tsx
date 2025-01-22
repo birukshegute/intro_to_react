a3ee7dcce24c5e319b9a4b552feeb7d2d43d0748
birukshegute <birukshegute@gmail.com>
2025-01-22 16:41:44 +0300

    fixing a few errors

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,13 +3,13 @@ function ListGroup() {
   items = [];
 
   const getMessage = () => {
-    items.length === 0 ? <p>No item found</p> : null;
+    return items.length === 0 ? <p>No item found</p> : null;
   };
 
   return (
     <>
       <h1>List</h1>
-      {getMessage}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}> {item}</li>
