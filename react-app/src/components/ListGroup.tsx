d72e1829226393907488af53b5a312646557b771
birukshegute <birukshegute@gmail.com>
2025-01-23 21:52:09 +0300

    first on click

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -7,7 +7,11 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
             {item}
           </li>
         ))}
