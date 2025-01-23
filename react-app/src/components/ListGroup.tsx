b5e46b650fa85a721d8b14570840b7f7736e86bc
birukshegute <birukshegute@gmail.com>
2025-01-23 22:12:12 +0300

    activating a member in a list

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,6 +2,7 @@ import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let selectedIndex = 0;
 
   const handleClick = (event: MouseEvent) => console.log(event);
 
@@ -10,8 +11,16 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
